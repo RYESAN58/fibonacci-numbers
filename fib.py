9dc853fc4430f92c70729c1ec18787fddffba430
Verified ✔️ <ryesan58@gmail.com>
2022-08-17 09:02:57 -0400

    Create fib.py

---
+++ fib.py
@@ -0,0 +1,26 @@
+
+
+
+
+def fibRecurs(n):
+    if(n <= 1):
+        return n
+    else:
+        return fibRecurs(n-1) + fibRecurs(n-2)
+
+print(fibRecurs(6))
+
+
+def fibLoop(n):
+    x = []
+    for i in range(n+1):
+        if i > 1:
+            x.append(x[i-1] + x[i-2])
+        elif i == 1:
+            x.append(1)
+        else:
+            x.append(0)
+    return x[n]
+
+
+print(fibLoop(400))
